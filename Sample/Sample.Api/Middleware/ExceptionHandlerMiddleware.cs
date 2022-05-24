23300bdfc7a9b5e06c44d090489919391befd68c
Bruno Ramos <brunoramos.bsilva@gmail.com>
2022-05-24 20:53:08 -0300

    implementando middleware

---
+++ Sample/Sample.Api/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using Sample.Application.Exceptions;
+using System.Net;
+
+namespace Sample.Api.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+
+        public ExceptionHandlerMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _requestDelegate(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            HttpStatusCode httpStatusCode = HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+            var result = string.Empty;
+            switch (exception)
+            {
+                case BadRequestException badRequestException:
+                    httpStatusCode = HttpStatusCode.BadRequest;
+                    result = badRequestException.Message;
+                    break;
+                case NotFoundException notFoundException:
+                    httpStatusCode = HttpStatusCode.NotFound;
+                    result = notFoundException.Message;
+                    break;
+                case Exception:
+                    httpStatusCode = HttpStatusCode.BadRequest;
+                    result = JsonConvert.SerializeObject(new { error = exception.Message });
+                    break;
+            }
+            context.Response.StatusCode = (int)httpStatusCode;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
