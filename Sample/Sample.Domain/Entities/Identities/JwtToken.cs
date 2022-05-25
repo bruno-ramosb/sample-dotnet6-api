34f151a104f290b7efe7958acb42b95514129456
Bruno Ramos <brunoramos.bsilva@gmail.com>
2022-05-24 21:19:34 -0300

    incluindo entidades identities

---
+++ Sample/Sample.Domain/Entities/Identities/JwtToken.cs
@@ -0,0 +1,9 @@
+﻿namespace Sample.Domain.Entities.Identities
+{
+    public class JwtToken
+    {
+        public string Token { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}
