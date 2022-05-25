83b8abcca58d9ce79d66fab20e109c7700163d77
Bruno Ramos <brunoramos.bsilva@gmail.com>
2022-05-24 21:10:59 -0300

    incluindo db context

---
+++ Sample/Sample.Persistence/DbContexts/SampleDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Audit.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sample.Persistence.DbContexts
+{
+    public class SampleDbContext : AuditDbContext
+    {
+        public SampleDbContext(DbContextOptions<SampleDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Sample/Sample.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sample.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DbContexts.SampleDbContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("PgsqlSampleContext"), sqloption =>
+                {
+                    sqloption.EnableRetryOnFailure(10, TimeSpan.FromSeconds(5), null);
+                });
+            });
+            return services;
+        }
+    }
+}
