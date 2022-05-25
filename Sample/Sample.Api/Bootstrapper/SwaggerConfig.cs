fe62117da63712cc86983e0bf7364dc89abd2107
Bruno Ramos <brunoramos.bsilva@gmail.com>
2022-05-25 13:48:42 -0300

    adicionando healthchek e swagger

---
+++ Sample/Sample.Api/Bootstrapper/SwaggerConfig.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Sample.Api.Bootstrapper
+{
+    public static class SwaggerConfig
+    {
+        public static void ConfigSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Example API",
+                    Version = "v1",
+                    Description = "API Example",
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+              {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                      }
+                });
+            });
+        }
+
+        public static void StartSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sample API");
+                c.RoutePrefix = "admin/Swagger";
+            });
+        }
+    }
+}
